2821d35a6739bd3299b0e4f676a515b1808aac58
Salman Dabbakuti <dabbakutisalman@gmail.com>
2023-09-25 12:12:20 +0530

    updated: using w3modal default connect button
    updated: using w3modal default connect button
    removed manual connect, isconnect functions
    set default chain as base in web3 modal initialization

--- client/app/components/SiteLayout.js
+++ client/app/components/SiteLayout.js
@@ -1,64 +1,30 @@
 "use client";
-import { useState } from "react";
-import { Layout } from "antd";
-import { useWeb3Modal } from "@web3modal/react";
-import { useAccount, useDisconnect } from "wagmi";
-import { Button, message } from "antd";
-import { WalletFilled } from "@ant-design/icons";
-import { base } from "wagmi/chains";
+import { Layout, Divider } from "antd";
+import { Web3Button } from "@web3modal/react";
 import "antd/dist/reset.css";
 
 const { Header, Footer, Content } = Layout;
 
 export default function SiteLayout({ children }) {
-  const [loading, setLoading] = useState({ connect: false });
-  const { address: account, isConnected } = useAccount();
-  const { disconnect } = useDisconnect();
-  const { open, setDefaultChain } = useWeb3Modal();
-
-  const handleWalletConnection = async () => {
-    setLoading({ connect: true });
-    try {
-      if (isConnected) return disconnectWallet();
-      await open();
-      setDefaultChain(base);
-    } catch (err) {
-      console.error("failed to connect wallet: ", err);
-      message.error("Failed to connect wallet");
-    } finally {
-      setLoading({ connect: false });
-    }
-  };
-
-  const disconnectWallet = () => {
-    disconnect();
-    message.success("Wallet disconnected");
-  };
-
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Header style={{ padding: 0 }}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            padding: "0 20px"
-          }}
-        >
-          <h3 style={{ color: "white" }}>Super Unlockable</h3>
-          <Button
-            type="primary"
-            onClick={handleWalletConnection}
-            icon={<WalletFilled />}
-            loading={loading?.connect}
-            style={{ borderRadius: 25 }}
-          >
-            {account
-              ? account.slice(0, 8) + "..." + account.slice(-5)
-              : "Connect Wallet"}
-          </Button>
-        </div>
+      <Header
+        style={{
+          padding: 0,
+          color: "#fff",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "0 10px"
+        }}
+      >
+        <h3>Super Unlockable</h3>
+        <Web3Button
+          balance="hide"
+          icon="show"
+          label="Connect Wallet"
+          avatar="show"
+        />
       </Header>
       <Content
         style={{
@@ -71,6 +37,7 @@ export default function SiteLayout({ children }) {
       >
         {children}
       </Content>
+      <Divider plain />
       <Footer style={{ textAlign: "center" }}>
         <a
           href="https://github.com/Salmandabbakuti"
--- client/app/components/Web3Provider.js
+++ client/app/components/Web3Provider.js
@@ -1,17 +1,19 @@
 "use client";
 import { Web3Modal } from "@web3modal/react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { base, baseGoerli } from "wagmi/chains";
+import { base } from "wagmi/chains";
 import {
   EthereumClient,
   w3mConnectors,
   w3mProvider
 } from "@web3modal/ethereum";
 
-const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "952483bf7a0f5ace4c40eb53967f1368";
-const supportedNetworks = [base, baseGoerli];
+const projectId =
+  process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ||
+  "952483bf7a0f5ace4c40eb53967f1368";
+const supportedChains = [base];
 
-const { publicClient } = configureChains(supportedNetworks, [
+const { publicClient } = configureChains(supportedChains, [
   w3mProvider({ projectId })
 ]);
 
@@ -19,20 +21,25 @@ const wagmiConfig = createConfig({
   autoConnect: false,
   connectors: w3mConnectors({
     projectId,
-    chains: supportedNetworks
+    chains: supportedChains
   }),
   publicClient
 });
-const ethereumClient = new EthereumClient(wagmiConfig, supportedNetworks);
-
+const ethereumClient = new EthereumClient(wagmiConfig, supportedChains);
 
 export default function Web3Provider({ children }) {
   return (
     <>
-      <WagmiConfig config={wagmiConfig}>
-        {children}
-      </WagmiConfig>
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+      <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+        defaultChain={base}
+        themeMode="light"
+        themeVariables={{
+          '--w3m-button-border-radius': '25px'
+        }}
+      />
     </>
   );
 }
